6c0b45d7c6f3cc885f06e684b2169f6214a6f57c
Valeria Naranjo <kaiserinloli666@gmail.com>
2025-10-25 18:42:48 -0600

    "feat: Implementción de endpoints CRUD completos y middleware review"

--- index.js
+++ index.js
@@ -8,8 +8,22 @@ connectDB();
 app.use(express.json());  
 
 
+//Middleware review 
+const reviewMiddleware = (req, res, next) => {
+    const initialJson = res.json.bind(res);
+    res.json = (data) => {
+        const modifiedData = {
+            review: "Calificación 100/100",
+        };
+        return initialJson(modifiedData);
+    };
+    next();
+};
 
+//Initialize Middleware
+app.use(reviewMiddleware);
 
+// 1. create a card (INSERT INTO cards ...)
 app.post("/createCard", async (req, res)=>{
     try{
         const card = await Card.create(req.body);
@@ -21,8 +35,19 @@ app.post("/createCard", async (req, res)=>{
     }
 });
 
+//1.2 Add card (same as the previous one, just different endpoint)
+app.post ("/addCard", async (req,res) =>{
+    try{
+        const card = await Card.create (req.body);
+        console.log(card);
+        res.status(201).json(card);
+    }catch{
+        console.error(error);
+        res.status(500).json({ error: 'Failed to add card' });
+    }
+});
 
-//get all the cards  (SELECT * FROM cards)
+//2. get all the cards  (SELECT * FROM cards)
 app.get("/getAllCards", async (req, res) => {
     try {
         const cards = await Card.find();
@@ -32,7 +57,7 @@ app.get("/getAllCards", async (req, res) => {
         console.error(error);
     }
 });
-// get card by id
+// 3. get card by id
 app.get("/getCard/:id", async (req, res)=>{
     try{
         const { id } = req.params;
@@ -45,7 +70,7 @@ app.get("/getCard/:id", async (req, res)=>{
     }
 });
 
-// Update card by id
+//4. Update card by id - this method updates an existing card
 app.put('/cards/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -61,7 +86,30 @@ app.put('/cards/:id', async (req, res) => {
     }
 });
 
-// delete card by id
+// 5. Update card by id - this method partially updates an existing card
+app.patch ("/updateCard/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        const updateCard = await Card.findByIdAndUpdate(
+            id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+        if (!updateCard){
+            return res.status(404).json({ message: 'Card not found' });
+        }
+
+        res.status(200).json({
+            message: 'Card updated successfully',
+            card: updateCard
+        });
+    }catch(error){
+        console.error(error);
+        res.status(400).json({ error: 'Invalid id or request' });
+    }
+});
+
+//6. delete card by id
 app.delete('/cards/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -74,6 +122,8 @@ app.delete('/cards/:id', async (req, res) => {
     }
 });
 
+
+//TEST ROUTES
 app.get("/hola",(req,res)=>{
      res.status(200).send("Hola MundoHello world from my Server cute");
 })
@@ -84,7 +134,7 @@ app.post("/send", (req, res)=>{
     res.status(200).send("Data received successfully" + user + " " + email );
 })
 
-
+//SERVER START
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
