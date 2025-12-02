1e7ff7ad713990beee5ca2a451c0aa5dba688678
Valeria Naranjo <kaiserinloli666@gmail.com>
2025-12-02 08:31:33 -0600

    "feat: typo changed"

--- models/Card.js
+++ models/Card.js
@@ -5,7 +5,7 @@ const cardSchema = new mongoose.Schema(
      name: {type: String, required: true, trim:true},
      link:{type: String, required: true, trim:true},
      description:{type: String, required: true},
-     like: {type:boolean, default:false},
+     like: {type:Boolean, default:false},
 },
 {
      timestamps: true, //fecha de creacion y actualizacion
